d1ed35fb02d755a48a245e4294b6ff0624944054
Ryan Bussert <ryanbussert19@gmail.com>
2024-02-28 11:26:37 -0500

    build

--- nodejs-production-env/routes/socketRoutes.js
+++
@@ -1,9 +0,0 @@
-const socketMethods = require('../methods/socketMethods')
-
-
-var socketRoutes = (socket) => {
-    console.log('New client connected');
-    socket.on('sendMessage', (query) => socketMethods.sendMessage(socket, query));
-}
-
-module.exports=socketRoutes
